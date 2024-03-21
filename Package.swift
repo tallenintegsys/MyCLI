d2b45d07411865192768f25abf1db6955f9a9238
tallenintegsys <tallen@integsys.biz>
2024-03-21 11:43:14 -0400

    I did a thing! - J Clarkson

--- Package.swift
+++ Package.swift
@@ -1,14 +1,22 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MyCLI",
+    dependencies: [
+      .package(url: "https://github.com/apple/example-package-figlet", branch: "main"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MyCLI"),
+            name: "MyCLI",
+            dependencies: [
+                .product(name: "Figlet", package: "example-package-figlet"),
+            ],
+            path: "Sources"),
     ]
 )
+
