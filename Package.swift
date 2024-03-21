5a3cb4ecf4b2b4d5e3df3df81f9343e16ea9c97c
tallenintegsys <tallen@integsys.biz>
2024-03-21 12:34:01 -0400

    add arg parsing

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "MyCLI",
     dependencies: [
       .package(url: "https://github.com/apple/example-package-figlet", branch: "main"),
+      .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -15,6 +16,7 @@ let package = Package(
             name: "MyCLI",
             dependencies: [
                 .product(name: "Figlet", package: "example-package-figlet"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
             ],
             path: "Sources"),
     ]
